2466cd456e2e6b44bf2f83fffde3c43026b09136
juanlopez1 <juan_manuel_lopez_1996@hotmail.com>
2024-12-09 06:51:09 -0300

    add info box in home page

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -4,6 +4,19 @@ const Home = () => (
     <div className="page__container">
         <h1>¡Bienvenido a VoteSeat!</h1>
         <p className="page__description">Calculá la distribución de escaños utilizando el sistema D'Hondt</p>
+        <div className="info-box" role="alert">
+            <h4 className="info-box__title">¿Qué es el sistema D'Hondt?</h4>
+            <p>
+                El sistema o método D'Hondt es un método de promedio mayor para asignar escaños en los sistemas de
+                representación proporcional por listas electorales. Los métodos de promedio mayor se caracterizan por
+                dividir mediante sucesivos divisores los totales de los votos obtenidos por los distintos partidos,
+                dando secuencias de cocientes decrecientes para cada partido y asignando los escaños a los promedios más
+                altos.{' '}
+                <a href="https://es.wikipedia.org/wiki/Sistema_D%27Hondt" target="_blank" rel="noreferrer">
+                    Más información.
+                </a>
+            </p>
+        </div>
         <div className="home-page-btn-container">
             <Button to="/calculate">Realizar cálculo</Button>
             <Button to="/history" style="outline">
